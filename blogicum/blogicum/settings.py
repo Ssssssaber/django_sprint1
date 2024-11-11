6d7007efd11d3420458e8fe86b0147aa10469295
Ssssssaber <bladimir.lobanov@gmail.com>
2024-11-11 17:38:31 +0700

    (fix) another pep8 fix

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'pages.apps.PagesConfig',
-    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
